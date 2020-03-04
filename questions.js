919f87805f21c83b87c10eb7d38f70d4656385b3
β︎Δℕ︎Σ β︎ℐ︎ⅅ︎ⅅ︎ℐ︎✕︎ ⛧ <1337god@cock.email>
2020-03-04 08:09:55 +0000

    Removido "n" fora do sítio

--- questions.js
+++ questions.js
@@ -4,7 +4,7 @@ questions =
             "Não há necessidade de um Governo central.",
             "As regiões devem ter seus próprios militares.", 
             "As leis das autarquias devem ter precedência sobre as leis nacionais."
-        ],n
+        ],
         [//3
             "As leis locais devem ser antepostas sobre as leis nacionais.",
             "O Governo central não consegue entender as necessidades do seu povo.",
