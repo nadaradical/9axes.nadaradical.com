0bb212e1f8c5b5e70c04681901b75f1232bb9a6f
Bane Biddix <1337god@cock.email>
2020-03-09 00:34:17 +0000

    Adicionado os valores do PS e algumas alterações
    PS - Feito pelo Militante do PS, Chico Duarte

--- parties.js
+++ parties.js
@@ -2,15 +2,15 @@ parties = [
     {
         "name": "Partido Socialista",
         "stats": {
-                        "divi": 0, //Separatismo vs. Unitarismo
-                        "govt": 0, //Anarquismo vs. Autoritarismo
-                        "dipl": 0, //Globalismo vs. Isolacionismo
-                        "mili": 0, //Militarismo vs. Pacifismo
-                        "prot": 0, //Controlo vs. Liberdade
-                        "econ": 0, //Intervencionismo vs. Livre Mercado
-                        "reli": 0, //Secular vs. Religioso
-                        "scty": 0, //Progressista vs. Conservador
-                        "cult": 0 //Assimilacionista vs. Multiculturalista
+                        "divi": 50, //Separatismo vs. Unitarismo
+                        "govt": 55, //Anarquismo vs. Autoritarismo
+                        "dipl": 90, //Globalismo vs. Isolacionismo
+                        "mili": 20, //Militarismo vs. Pacifismo
+                        "prot": 45, //Controlo vs. Liberdade
+                        "econ": 70, //Intervencionismo vs. Livre Mercado
+                        "reli": 90, //Secular vs. Religioso
+                        "scty": 85, //Progressista vs. Conservador
+                        "cult": 20 //Assimilacionista vs. Multiculturalista
         }
     },
     {
