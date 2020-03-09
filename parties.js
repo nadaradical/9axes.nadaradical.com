b12fa4f9c4a39e4b065c63f31298f49a6704be77
Bane Biddix <1337god@cock.email>
2020-03-09 00:12:42 +0000

    Partidos Removidos
    Apenas deixar todos os partidos que têm representantes eleitos na Assembleia da República.
    Exceção: Livre ficou mas, já não têm representantes pois ficou sem a deputada no Parlamento depois de lhe retirar a confiança política.

--- parties.js
+++ parties.js
@@ -84,7 +84,7 @@ parties = [
         }
     },
     {
-        "name": "   Partido Ecologista - Os Verdes",
+        "name": "Partido Ecologista - Os Verdes",
         "stats": {
                         "divi": 0, //Separatismo vs. Unitarismo
                         "govt": 0, //Anarquismo vs. Autoritarismo
@@ -113,48 +113,6 @@ parties = [
     },
     {
         "name": "Iniciativa Liberal",
-        "stats": {
-                        "divi": 0, //Separatismo vs. Unitarismo
-                        "govt": 0, //Anarquismo vs. Autoritarismo
-                        "dipl": 0, //Globalismo vs. Isolacionismo
-                        "mili": 0, //Militarismo vs. Pacifismo
-                        "prot": 0, //Controlo vs. Liberdade
-                        "econ": 0, //Intervencionismo vs. Livre Mercado
-                        "reli": 0, //Secular vs. Religioso
-                        "scty": 0, //Progressista vs. Conservador
-                        "cult": 0 //Assimilacionista vs. Multiculturalista
-        }
-    },
-    {
-        "name": "Juntos pelo Povo",
-        "stats": {
-                        "divi": 0, //Separatismo vs. Unitarismo
-                        "govt": 0, //Anarquismo vs. Autoritarismo
-                        "dipl": 0, //Globalismo vs. Isolacionismo
-                        "mili": 0, //Militarismo vs. Pacifismo
-                        "prot": 0, //Controlo vs. Liberdade
-                        "econ": 0, //Intervencionismo vs. Livre Mercado
-                        "reli": 0, //Secular vs. Religioso
-                        "scty": 0, //Progressista vs. Conservador
-                        "cult": 0 //Assimilacionista vs. Multiculturalista
-        }
-    },
-    {
-        "name": "Partido Popular Monárquico",
-        "stats": {
-                        "divi": 0, //Separatismo vs. Unitarismo
-                        "govt": 0, //Anarquismo vs. Autoritarismo
-                        "dipl": 0, //Globalismo vs. Isolacionismo
-                        "mili": 0, //Militarismo vs. Pacifismo
-                        "prot": 0, //Controlo vs. Liberdade
-                        "econ": 0, //Intervencionismo vs. Livre Mercado
-                        "reli": 0, //Secular vs. Religioso
-                        "scty": 0, //Progressista vs. Conservador
-                        "cult": 0 //Assimilacionista vs. Multiculturalista
-        }
-    },
-    {
-        "name": "Nós, Cidadãos!",
         "stats": {
                         "divi": 0, //Separatismo vs. Unitarismo
                         "govt": 0, //Anarquismo vs. Autoritarismo
