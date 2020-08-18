0cfc61d2b3e85463b3568a8ad18be730aec7b0d9
Bane Biddix <bane@wrongthink.net>
2020-08-18 13:55:51 +0100

    Partidos Alterados e Adicionados

--- parties.js
+++ parties.js
@@ -2,141 +2,154 @@ parties = [
     {
         "name": "Partido Socialista",
         "stats": {
-                        "divi": 50, //Separatismo vs. Unitarismo
-                        "govt": 55, //Anarquismo vs. Autoritarismo
+                        "divi": 40, //Separatismo vs. Unitarismo
+                        "govt": 45, //Anarquismo vs. Autoritarismo
                         "dipl": 90, //Globalismo vs. Isolacionismo
                         "mili": 20, //Militarismo vs. Pacifismo
-                        "prot": 45, //Controlo vs. Liberdade
+                        "prot": 55, //Controlo vs. Liberdade
                         "econ": 70, //Intervencionismo vs. Livre Mercado
-                        "reli": 90, //Secular vs. Religioso
-                        "scty": 85, //Progressista vs. Conservador
+                        "reli": 80, //Secular vs. Religioso
+                        "scty": 75, //Progressista vs. Conservador
                         "cult": 20 //Assimilacionista vs. Multiculturalista
         }
     },
     {
         "name": "Partido Social Democrata",
         "stats": {
-                        "divi": 65, //Separatismo vs. Unitarismo
+                        "divi": 50, //Separatismo vs. Unitarismo
                         "govt": 45, //Anarquismo vs. Autoritarismo
                         "dipl": 70, //Globalismo vs. Isolacionismo
-                        "mili": 40, //Militarismo vs. Pacifismo
-                        "prot": 35, //Controlo vs. Liberdade
-                        "econ": 35, //Intervencionismo vs. Livre Mercado
-                        "reli": 75, //Secular vs. Religioso
+                        "mili": 30, //Militarismo vs. Pacifismo
+                        "prot": 40, //Controlo vs. Liberdade
+                        "econ": 45, //Intervencionismo vs. Livre Mercado
+                        "reli": 70, //Secular vs. Religioso
                         "scty": 40, //Progressista vs. Conservador
-                        "cult": 45 //Assimilacionista vs. Multiculturalista
+                        "cult": 35 //Assimilacionista vs. Multiculturalista
         }
     },
     {
         "name": "Bloco de Esquerda",
         "stats": {
-                        "divi": 0, //Separatismo vs. Unitarismo
-                        "govt": 0, //Anarquismo vs. Autoritarismo
-                        "dipl": 0, //Globalismo vs. Isolacionismo
-                        "mili": 0, //Militarismo vs. Pacifismo
-                        "prot": 0, //Controlo vs. Liberdade
-                        "econ": 0, //Intervencionismo vs. Livre Mercado
-                        "reli": 0, //Secular vs. Religioso
-                        "scty": 0, //Progressista vs. Conservador
-                        "cult": 0 //Assimilacionista vs. Multiculturalista
+                        "divi": 60, //Separatismo vs. Unitarismo
+                        "govt": 40, //Anarquismo vs. Autoritarismo
+                        "dipl": 70, //Globalismo vs. Isolacionismo
+                        "mili": 15, //Militarismo vs. Pacifismo
+                        "prot": 60, //Controlo vs. Liberdade
+                        "econ": 80, //Intervencionismo vs. Livre Mercado
+                        "reli": 95, //Secular vs. Religioso
+                        "scty": 90, //Progressista vs. Conservador
+                        "cult": 15 //Assimilacionista vs. Multiculturalista
         }
     },
     {
         "name": "Partido Comunista Português",
         "stats": {
-                        "divi": 0, //Separatismo vs. Unitarismo
-                        "govt": 40, //Anarquismo vs. Autoritarismo
-                        "dipl": 80, //Globalismo vs. Isolacionismo
-                        "mili": 50, //Militarismo vs. Pacifismo
-                        "prot": 20, //Controlo vs. Liberdade
-                        "econ": 100, //Intervencionismo vs. Livre Mercado
-                        "reli": 80, //Secular vs. Religioso
-                        "scty": 60, //Progressista vs. Conservador
-                        "cult": 20 //Assimilacionista vs. Multiculturalista
+                        "divi": 5, //Separatismo vs. Unitarismo
+                        "govt": 20, //Anarquismo vs. Autoritarismo
+                        "dipl": 30, //Globalismo vs. Isolacionismo
+                        "mili": 85, //Militarismo vs. Pacifismo
+                        "prot": 80, //Controlo vs. Liberdade
+                        "econ": 95, //Intervencionismo vs. Livre Mercado
+                        "reli": 70, //Secular vs. Religioso
+                        "scty": 30, //Progressista vs. Conservador
+                        "cult": 25 //Assimilacionista vs. Multiculturalista
         }
     },
     {
         "name": "CDS - Partido Popular",
         "stats": {
-                        "divi": 0, //Separatismo vs. Unitarismo
-                        "govt": 0, //Anarquismo vs. Autoritarismo
-                        "dipl": 0, //Globalismo vs. Isolacionismo
-                        "mili": 0, //Militarismo vs. Pacifismo
-                        "prot": 0, //Controlo vs. Liberdade
-                        "econ": 0, //Intervencionismo vs. Livre Mercado
-                        "reli": 0, //Secular vs. Religioso
-                        "scty": 0, //Progressista vs. Conservador
-                        "cult": 0 //Assimilacionista vs. Multiculturalista
+                        "divi": 40, //Separatismo vs. Unitarismo
+                        "govt": 40, //Anarquismo vs. Autoritarismo
+                        "dipl": 70, //Globalismo vs. Isolacionismo
+                        "mili": 40, //Militarismo vs. Pacifismo
+                        "prot": 45, //Controlo vs. Liberdade
+                        "econ": 35, //Intervencionismo vs. Livre Mercado
+                        "reli": 20, //Secular vs. Religioso
+                        "scty": 20, //Progressista vs. Conservador
+                        "cult": 20 //Assimilacionista vs. Multiculturalista
         }
     },
     {
         "name": "Pessoas-Animais-Natureza",
         "stats": {
-                        "divi": 0, //Separatismo vs. Unitarismo
-                        "govt": 0, //Anarquismo vs. Autoritarismo
-                        "dipl": 0, //Globalismo vs. Isolacionismo
-                        "mili": 0, //Militarismo vs. Pacifismo
-                        "prot": 0, //Controlo vs. Liberdade
-                        "econ": 0, //Intervencionismo vs. Livre Mercado
-                        "reli": 0, //Secular vs. Religioso
-                        "scty": 0, //Progressista vs. Conservador
-                        "cult": 0 //Assimilacionista vs. Multiculturalista
-        }
-    },
-    {
-        "name": "Partido Ecologista - Os Verdes",
-        "stats": {
-                        "divi": 0, //Separatismo vs. Unitarismo
-                        "govt": 0, //Anarquismo vs. Autoritarismo
-                        "dipl": 0, //Globalismo vs. Isolacionismo
-                        "mili": 0, //Militarismo vs. Pacifismo
-                        "prot": 0, //Controlo vs. Liberdade
-                        "econ": 0, //Intervencionismo vs. Livre Mercado
-                        "reli": 0, //Secular vs. Religioso
-                        "scty": 0, //Progressista vs. Conservador
-                        "cult": 0 //Assimilacionista vs. Multiculturalista
+                        "divi": 70, //Separatismo vs. Unitarismo
+                        "govt": 75, //Anarquismo vs. Autoritarismo
+                        "dipl": 85, //Globalismo vs. Isolacionismo
+                        "mili": 10, //Militarismo vs. Pacifismo
+                        "prot": 40, //Controlo vs. Liberdade
+                        "econ": 55, //Intervencionismo vs. Livre Mercado
+                        "reli": 85, //Secular vs. Religioso
+                        "scty": 75, //Progressista vs. Conservador
+                        "cult": 20 //Assimilacionista vs. Multiculturalista
         }
     },
     {
         "name": "CHEGA",
         "stats": {
-                        "divi": 0, //Separatismo vs. Unitarismo
-                        "govt": 0, //Anarquismo vs. Autoritarismo
-                        "dipl": 0, //Globalismo vs. Isolacionismo
-                        "mili": 0, //Militarismo vs. Pacifismo
-                        "prot": 0, //Controlo vs. Liberdade
-                        "econ": 0, //Intervencionismo vs. Livre Mercado
-                        "reli": 0, //Secular vs. Religioso
-                        "scty": 0, //Progressista vs. Conservador
-                        "cult": 0 //Assimilacionista vs. Multiculturalista
+                        "divi": 15, //Separatismo vs. Unitarismo
+                        "govt": 20, //Anarquismo vs. Autoritarismo
+                        "dipl": 30, //Globalismo vs. Isolacionismo
+                        "mili": 65, //Militarismo vs. Pacifismo
+                        "prot": 80, //Controlo vs. Liberdade
+                        "econ": 25, //Intervencionismo vs. Livre Mercado
+                        "reli": 25, //Secular vs. Religioso
+                        "scty": 20, //Progressista vs. Conservador
+                        "cult": 80 //Assimilacionista vs. Multiculturalista
         }
     },
     {
         "name": "Iniciativa Liberal",
         "stats": {
-                        "divi": 70, //Separatismo vs. Unitarismo
+                        "divi": 75, //Separatismo vs. Unitarismo
                         "govt": 75, //Anarquismo vs. Autoritarismo
-                        "dipl": 50, //Globalismo vs. Isolacionismo
-                        "mili": 25, //Militarismo vs. Pacifismo
-                        "prot": 0, //Controlo vs. Liberdade
-                        "econ": 10, //Intervencionismo vs. Livre Mercado
-                        "reli": 50, //Secular vs. Religioso
-                        "scty": 60, //Progressista vs. Conservador
+                        "dipl": 85, //Globalismo vs. Isolacionismo
+                        "mili": 20, //Militarismo vs. Pacifismo
+                        "prot": 25, //Controlo vs. Liberdade
+                        "econ": 20, //Intervencionismo vs. Livre Mercado
+                        "reli": 85, //Secular vs. Religioso
+                        "scty": 65, //Progressista vs. Conservador
                         "cult": 20 //Assimilacionista vs. Multiculturalista
+        }
+    },
+        "name": "Partido Libertário",
+        "stats": {
+                        "divi": 100, //Separatismo vs. Unitarismo
+                        "govt": 100, //Anarquismo vs. Autoritarismo
+                        "dipl": 100, //Globalismo vs. Isolacionismo
+                        "mili": 0, //Militarismo vs. Pacifismo
+                        "prot": 0, //Controlo vs. Liberdade
+                        "econ": 0, //Intervencionismo vs. Livre Mercado
+                        "reli": 100, //Secular vs. Religioso
+                        "scty": 100, //Progressista vs. Conservador
+                        "cult": 0 //Assimilacionista vs. Multiculturalista
+        }
+    },
+    {
+        "name": "Volt",
+        "stats": {
+                        "divi": 80, //Separatismo vs. Unitarismo
+                        "govt": 60, //Anarquismo vs. Autoritarismo
+                        "dipl": 90, //Globalismo vs. Isolacionismo
+                        "mili": 15, //Militarismo vs. Pacifismo
+                        "prot": 45, //Controlo vs. Liberdade
+                        "econ": 55, //Intervencionismo vs. Livre Mercado
+                        "reli": 80, //Secular vs. Religioso
+                        "scty": 90, //Progressista vs. Conservador
+                        "cult": 5 //Assimilacionista vs. Multiculturalista
         }
     },
     {
         "name": "LIVRE",
         "stats": {
-                        "divi": 0, //Separatismo vs. Unitarismo
-                        "govt": 0, //Anarquismo vs. Autoritarismo
-                        "dipl": 0, //Globalismo vs. Isolacionismo
-                        "mili": 0, //Militarismo vs. Pacifismo
-                        "prot": 0, //Controlo vs. Liberdade
-                        "econ": 0, //Intervencionismo vs. Livre Mercado
-                        "reli": 0, //Secular vs. Religioso
-                        "scty": 0, //Progressista vs. Conservador
-                        "cult": 0 //Assimilacionista vs. Multiculturalista
+                        "divi": 90, //Separatismo vs. Unitarismo
+                        "govt": 80, //Anarquismo vs. Autoritarismo
+                        "dipl": 80, //Globalismo vs. Isolacionismo
+                        "mili": 15, //Militarismo vs. Pacifismo
+                        "prot": 25, //Controlo vs. Liberdade
+                        "econ": 85, //Intervencionismo vs. Livre Mercado
+                        "reli": 85, //Secular vs. Religioso
+                        "scty": 90, //Progressista vs. Conservador
+                        "cult": 10 //Assimilacionista vs. Multiculturalista
         }
     }
 ];
