7334aedc986211a1bd2af009d57e5b49bbaea1b9
joker@home:~$ whoami <joker@nadaradical.com>
2026-03-12 13:43:46 +0000

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,4 +1,5 @@
-ideologies = [{
+ideologies = [
+    {
         "name": "Clássico Liberal",
         "stats": {
             "divi": 70,
@@ -80,160 +81,162 @@ ideologies = [{
             "reli": 45,
             "scty": 35,
             "cult": 70
-        },
-        {
-            "name": "Neoliberal",
-            "stats": {
-                "divi": 65,
-                "govt": 60,
-                "dipl": 90,
-                "mili": 35,
-                "prot": 35,
-                "econ": 10,
-                "reli": 75,
-                "scty": 60,
-                "cult": 35
-            }
-        },
-        {
-            "name": "Democrata Cristão",
-            "stats": {
-                "divi": 45,
-                "govt": 40,
-                "dipl": 65,
-                "mili": 40,
-                "prot": 45,
-                "econ": 50,
-                "reli": 20,
-                "scty": 40,
-                "cult": 60
-            }
-        },
-        {
-            "name": "Distributista",
-            "stats": {
-                "divi": 55,
-                "govt": 45,
-                "dipl": 50,
-                "mili": 30,
-                "prot": 45,
-                "econ": 60,
-                "reli": 15,
-                "scty": 35,
-                "cult": 55
-            }
-        },
-        {
-            "name": "Ecologista",
-            "stats": {
-                "divi": 55,
-                "govt": 40,
-                "dipl": 80,
-                "mili": 10,
-                "prot": 40,
-                "econ": 70,
-                "reli": 85,
-                "scty": 80,
-                "cult": 30
-            }
-        },
-        {
-            "name": "Anarquista",
-            "stats": {
-                "divi": 100,
-                "govt": 100,
-                "dipl": 85,
-                "mili": 10,
-                "prot": 0,
-                "econ": 70,
-                "reli": 90,
-                "scty": 90,
-                "cult": 25
-            }
-        },
-        {
-            "name": "Anarcocapitalista",
-            "stats": {
-                "divi": 100,
-                "govt": 100,
-                "dipl": 90,
-                "mili": 5,
-                "prot": 0,
-                "econ": 0,
-                "reli": 95,
-                "scty": 80,
-                "cult": 20
-            }
-        },
-        {
-            "name": "Fascista",
-            "stats": {
-                "divi": 5,
-                "govt": 5,
-                "dipl": 10,
-                "mili": 95,
-                "prot": 95,
-                "econ": 45,
-                "reli": 30,
-                "scty": 5,
-                "cult": 95
-            }
-        },
-        {
-            "name": "Comunista Autoritário",
-            "stats": {
-                "divi": 10,
-                "govt": 5,
-                "dipl": 70,
-                "mili": 75,
-                "prot": 90,
-                "econ": 100,
-                "reli": 95,
-                "scty": 70,
-                "cult": 50
-            }
-        },
-        {
-            "name": "Monárquico",
-            "stats": {
-                "divi": 10,
-                "govt": 15,
-                "dipl": 40,
-                "mili": 60,
-                "prot": 70,
-                "econ": 45,
-                "reli": 20,
-                "scty": 20,
-                "cult": 70
-            }
-        },
-        {
-            "name": "Tecnocrata",
-            "stats": {
-                "divi": 40,
-                "govt": 30,
-                "dipl": 80,
-                "mili": 30,
-                "prot": 60,
-                "econ": 40,
-                "reli": 90,
-                "scty": 50,
-                "cult": 40
-            }
-        },
-        {
-            "name": "Populista",
-            "stats": {
-                "divi": 35,
-                "govt": 35,
-                "dipl": 35,
-                "mili": 60,
-                "prot": 60,
-                "econ": 55,
-                "reli": 40,
-                "scty": 40,
-                "cult": 60
-            }
+        }
+    },
+    {
+        "name": "Neoliberal",
+        "stats": {
+            "divi": 65,
+            "govt": 60,
+            "dipl": 90,
+            "mili": 35,
+            "prot": 35,
+            "econ": 10,
+            "reli": 75,
+            "scty": 60,
+            "cult": 35
+        }
+    },
+    {
+        "name": "Democrata Cristão",
+        "stats": {
+            "divi": 45,
+            "govt": 40,
+            "dipl": 65,
+            "mili": 40,
+            "prot": 45,
+            "econ": 50,
+            "reli": 20,
+            "scty": 40,
+            "cult": 60
+        }
+    },
+    {
+        "name": "Distributista",
+        "stats": {
+            "divi": 55,
+            "govt": 45,
+            "dipl": 50,
+            "mili": 30,
+            "prot": 45,
+            "econ": 60,
+            "reli": 15,
+            "scty": 35,
+            "cult": 55
+        }
+    },
+    {
+        "name": "Ecologista",
+        "stats": {
+            "divi": 55,
+            "govt": 40,
+            "dipl": 80,
+            "mili": 10,
+            "prot": 40,
+            "econ": 70,
+            "reli": 85,
+            "scty": 80,
+            "cult": 30
+        }
+    },
+    {
+        "name": "Anarquista",
+        "stats": {
+            "divi": 100,
+            "govt": 100,
+            "dipl": 85,
+            "mili": 10,
+            "prot": 0,
+            "econ": 70,
+            "reli": 90,
+            "scty": 90,
+            "cult": 25
+        }
+    },
+    {
+        "name": "Anarcocapitalista",
+        "stats": {
+            "divi": 100,
+            "govt": 100,
+            "dipl": 90,
+            "mili": 5,
+            "prot": 0,
+            "econ": 0,
+            "reli": 95,
+            "scty": 80,
+            "cult": 20
+        }
+    },
+    {
+        "name": "Fascista",
+        "stats": {
+            "divi": 5,
+            "govt": 5,
+            "dipl": 10,
+            "mili": 95,
+            "prot": 95,
+            "econ": 45,
+            "reli": 30,
+            "scty": 5,
+            "cult": 95
+        }
+    },
+    {
+        "name": "Comunista Autoritário",
+        "stats": {
+            "divi": 10,
+            "govt": 5,
+            "dipl": 70,
+            "mili": 75,
+            "prot": 90,
+            "econ": 100,
+            "reli": 95,
+            "scty": 70,
+            "cult": 50
+        }
+    },
+    {
+        "name": "Monárquico",
+        "stats": {
+            "divi": 10,
+            "govt": 15,
+            "dipl": 40,
+            "mili": 60,
+            "prot": 70,
+            "econ": 45,
+            "reli": 20,
+            "scty": 20,
+            "cult": 70
+        }
+    },
+    {
+        "name": "Tecnocrata",
+        "stats": {
+            "divi": 40,
+            "govt": 30,
+            "dipl": 80,
+            "mili": 30,
+            "prot": 60,
+            "econ": 40,
+            "reli": 90,
+            "scty": 50,
+            "cult": 40
+        }
+    },
+    {
+        "name": "Populista",
+        "stats": {
+            "divi": 35,
+            "govt": 35,
+            "dipl": 35,
+            "mili": 60,
+            "prot": 60,
+            "econ": 55,
+            "reli": 40,
+            "scty": 40,
+            "cult": 60
         }
     }
 ];
+results.html
+results.html
