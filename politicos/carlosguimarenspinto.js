2019c4a8bcdb2391ed0949ef375906731d927c24
joker@home:~$ whoami <joker@nadaradical.com>
2026-03-12 15:25:09 +0000

    Redesign politician page header and improve title

---
+++ politicos/carlosguimarenspinto.js
@@ -0,0 +1,17 @@
+window.politicoData = {
+  slug: "carlosguimarenspinto",
+  nome: "Carlos Guimarães Pinto",
+  partido: "Iniciativa Liberal",
+  foto: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Carlos_Guimaraes_Pinto_2024.jpg/512px-Carlos_Guimaraes_Pinto_2024.jpg",
+  resultados: {
+    divi: 75,
+    govt: 75,
+    dipl: 85,
+    mili: 20,
+    prot: 25,
+    econ: 20,
+    reli: 85,
+    scty: 65,
+    cult: 20
+  }
+};
