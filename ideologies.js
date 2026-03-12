73d5b576e367f1c346dfc53afbf5f375924f10b9
joker@home:~$ whoami <joker@nadaradical.com>
2026-03-12 13:05:52 +0000

    Create ideologies.js
    Adicionar ideologias para apresentar as pessoas.

---
+++ ideologies.js
@@ -0,0 +1,86 @@
+ideologies = [
+    {
+        "name": "Clássico Liberal",
+        "stats": {
+            "divi": 70,
+            "govt": 75,
+            "dipl": 85,
+            "mili": 25,
+            "prot": 20,
+            "econ": 20,
+            "reli": 80,
+            "scty": 65,
+            "cult": 20
+        }
+    },
+    {
+        "name": "Social-Democrata",
+        "stats": {
+            "divi": 50,
+            "govt": 45,
+            "dipl": 70,
+            "mili": 25,
+            "prot": 45,
+            "econ": 65,
+            "reli": 75,
+            "scty": 60,
+            "cult": 25
+        }
+    },
+    {
+        "name": "Conservador Nacional",
+        "stats": {
+            "divi": 25,
+            "govt": 30,
+            "dipl": 30,
+            "mili": 75,
+            "prot": 75,
+            "econ": 35,
+            "reli": 25,
+            "scty": 25,
+            "cult": 75
+        }
+    },
+    {
+        "name": "Socialista Democrático",
+        "stats": {
+            "divi": 55,
+            "govt": 45,
+            "dipl": 75,
+            "mili": 20,
+            "prot": 55,
+            "econ": 85,
+            "reli": 85,
+            "scty": 85,
+            "cult": 20
+        }
+    },
+    {
+        "name": "Libertário",
+        "stats": {
+            "divi": 95,
+            "govt": 95,
+            "dipl": 95,
+            "mili": 5,
+            "prot": 5,
+            "econ": 5,
+            "reli": 95,
+            "scty": 85,
+            "cult": 10
+        }
+    },
+    {
+        "name": "Comunitarista",
+        "stats": {
+            "divi": 35,
+            "govt": 35,
+            "dipl": 55,
+            "mili": 40,
+            "prot": 60,
+            "econ": 60,
+            "reli": 45,
+            "scty": 35,
+            "cult": 70
+        }
+    }
+];
